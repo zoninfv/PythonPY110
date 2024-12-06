11072450519336a45e3f3a0f33cc4d13b1afc3e0
zoninfv <zonin.fv@gmail.com>
2024-12-06 21:03:05 +0300

    06.12 21-02

---
+++ weather_api.py
@@ -0,0 +1,57 @@
+import requests
+from datetime import datetime
+
+# Словарь перевода значений направления ветра
+DIRECTION_TRANSFORM = {
+    'n': 'северное',
+    'nne': 'северо - северо - восточное',
+    'ne': 'северо - восточное',
+    'ene': 'восточно - северо - восточное',
+    'e': 'восточное',
+    'ese': 'восточно - юго - восточное',
+    'se': 'юго - восточное',
+    'sse': 'юго - юго - восточное',
+    's': 'южное',
+    'ssw': 'юго - юго - западное',
+    'sw': 'юго - западное',
+    'wsw': 'западно - юго - западное',
+    'w': 'западное',
+    'wnw': 'западно - северо - западное',
+    'nw': 'северо - западное',
+    'nnw': 'северо - северо - западное',
+    'c': 'штиль',
+}
+
+
+def current_weather(lat, lon):
+    """
+    Описание функции, входных и выходных переменных
+    """
+    token = '7979192f-06e2-4992-8da4-b7c51517c89c'  # Вставить ваш токен
+    url = f"https://api.weather.yandex.ru/v2/forecast?lat={lat}&lon={lon}"  # Если вдруг используете тариф «Погода на вашем сайте»
+    # то вместо forecast используйте informers. url = f"https://api.weather.yandex.ru/v2/informers?lat={lat}&lon={lon}"
+    headers = {"X-Yandex-API-Key": f"{token}"}
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    #print(data)
+
+    # Данная реализация приведена для тарифа «Тестовый», если у вас Тариф «Погода на вашем сайте», то закомментируйте пару строк указанных ниже
+    result = {
+        'temp': data['fact']['temp'],
+         # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
+        'time': datetime.fromtimestamp(data['fact']['uptime']).strftime("%H:%M"),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
+        # 'city': data['geo_object']['locality']['name'],# TODO Реализовать вычисление температуры из данных полученных от API
+        'feels_like_temp': data['fact']['feels_like'],  # TODO Реализовать вычисление ощущаемой температуры из данных полученных от API
+        'pressure': data['fact']['pressure_mm'],  # TODO Реализовать вычисление давления из данных полученных от API
+        'humidity': data['fact']['humidity'],  # TODO Реализовать вычисление влажности из данных полученных от API
+        'wind_speed': data['fact']['wind_speed'],  # TODO Реализовать вычисление скорости ветра из данных полученных от API
+        'wind_gust': data['fact']['wind_gust'],  # TODO Реализовать вычисление скорости порывов ветка из данных полученных от API
+        'wind_dir': DIRECTION_TRANSFORM.get(data['fact']['wind_dir']),  # Если используете Тариф «Погода на вашем сайте», то закомментируйте эту строку
+    }
+    return result
+
+
+if __name__ == "__main__":
+    print(current_weather(59.93, 30.34))  # Проверка работы для координат Санкт-Петербурга
+
+
