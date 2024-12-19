1ecd072ff7bc59d696500f1712dff532683738e4
zonin.fv <zonin.fv@gmail.com>
2024-12-19 23:44:28 +0300

    19.12

--- store/views.py
+++ store/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
-
+#from django.shortcuts import render
+#from models import DATABASE
 # Create your views here.
 
-
+from django.http import HttpResponseNotFound
 from django.http import JsonResponse
 from .models import DATABASE
 def products_view(request):
     if request.method == "GET":
-        ID
+        if request.GET.get('id'):
+            ID = request.GET['id']
+            if ID in DATABASE:
+                return JsonResponse(DATABASE[ID],json_dumps_params={'ensure_ascii': False,
+                                                             'indent': 4})
+            else:
+                return HttpResponseNotFound("Данного продукта нет в базе данных")
+        else:
+            return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
+                                                             'indent': 4})
 
 
 
 
-        return JsonResponse(DATABASE, json_dumps_params={'ensure_ascii': False,
-                                                     'indent': 4})
+
 
 from django.http import HttpResponse, HttpResponseNotFound
 
